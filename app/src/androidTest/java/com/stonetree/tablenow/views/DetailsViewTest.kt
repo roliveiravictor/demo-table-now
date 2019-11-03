4c9cdff2e926ed079199c79844b6289d224aae60
Victor R <victor.rocha@mercadolivre.com>
2019-11-03 18:19:34 -0300

    added details view test

---
+++ app/src/androidTest/java/com/stonetree/tablenow/views/DetailsViewTest.kt
@@ -0,0 +1,71 @@
+package com.stonetree.tablenow.views
+
+import android.content.pm.ActivityInfo.*
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.espresso.matcher.ViewMatchers.Visibility.*
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
+import com.stonetree.tablenow.R
+import com.stonetree.tablenow.constants.Constants
+import com.stonetree.tablenow.mocks.Mocks
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class DetailsViewTest {
+
+    @Rule
+    @JvmField
+    val rule = ActivityTestRule(NavigatorActivity::class.java)
+
+    @Before
+    fun setup() {
+        rule.activity.findNavController(R.id.navigator).apply {
+            navigate(
+                R.id.view_details, Bundle().apply {
+                    putSerializable(Constants.Bundle.MERCHANT, Mocks.merchant)
+                }
+            )
+        }
+    }
+
+    @Test
+    fun screenRotation_shouldReturnPagerVisible() {
+        rule.activity.requestedOrientation = SCREEN_ORIENTATION_PORTRAIT
+        onView(withId(R.id.banner_pager)).check(
+            matches(
+                withEffectiveVisibility(
+                    VISIBLE
+                )
+            )
+        )
+    }
+
+    @Test
+    fun screenRotation_shouldReturnPagerGone() {
+        rule.activity.requestedOrientation = SCREEN_ORIENTATION_LANDSCAPE
+        onView(withId(R.id.banner_pager)).check(
+            matches(
+                withEffectiveVisibility(
+                    GONE
+                )
+            )
+        )
+    }
+
+    @Test
+    fun screenRotation_shouldRetainData() {
+        rule.activity.requestedOrientation = SCREEN_ORIENTATION_LANDSCAPE
+        onView(withId(R.id.title)).check(
+            matches(
+                withText("mName")
+            )
+        )
+    }
+}
